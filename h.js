581f2ad3a45e4fea756c33de9a4cbe04e59454b9
SidMorrisMw <bis22-mnkhoma@mubas.ac.mw>
2025-08-05 10:50:38 +0200

    first commit

---
+++ h.js
@@ -0,0 +1,14 @@
+
+
+
+
+ const h1=document.createElement("h1");
+
+ h1.textContent="HELLO MAN!";
+
+
+ const li1=document.createElement("li");
+ li1.textContent="pineapple";
+ 
+
+ document.ol.appendChild("l1");
